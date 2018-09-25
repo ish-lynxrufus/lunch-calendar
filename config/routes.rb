961fcb50edad41f893115134828fc4c24294de69
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-09-25 14:53:06 +0900

    カレンダーイベント取得のモック実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/calendar' do
+    get '/redirect', to: 'calendar#redirect', as: 'calendar_redirect'
+    get '/callback', to: 'calendar#callback', as: 'calendar_callback'
+    get '/events/', to: 'calendar#events', as: 'calendar_events'
+  end
 end
