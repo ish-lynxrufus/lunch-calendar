1278977e752eae64ee633051fba880b5e6592c86
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-09-26 11:44:07 +0900

    LinebotControllerの命名変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   scope '/linebot' do
-    post '/callback/', to: 'linebot#callback', as: 'linebot_callback'
-    get '/push/', to: 'linebot#push', as: 'linebot_push'
+    post '/callback/', to: 'line_bot#callback', as: 'line_bot_callback'
+    get '/push/', to: 'line_bot#push', as: 'line_bot_push'
   end
 end
